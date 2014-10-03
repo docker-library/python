9cdb6eeb857908d3817cdd46f63e3b954f3072ae
Tianon Gravi <admwiggin@gmail.com>
2014-10-03 14:52:08 -0600

    Fix update.sh to not mistakenly capture RCs (and thus push 3.4 down to 3.4.1, since 3.4.2 isn't actually out yet)

--- update.sh
+++ update.sh
@@ -10,7 +10,9 @@ fi
 versions=( "${versions[@]%/}" )
 
 for version in "${versions[@]}"; do
-	fullVersion="$(curl -sSL 'https://www.python.org/ftp/python/' | grep '<a href="'"$version." | sed -r 's!.*<a href="([^"/]+)/?".*!\1!' | sort -V | tail -1)"
+	# <span class="release-number"><a href="/downloads/release/python-278/">Python 2.7.8</a></span>
+	# <span class="release-number"><a href="/downloads/release/python-341/">Python 3.4.1</a></span>
+	fullVersion="$(curl -sSL 'https://www.python.org/downloads/' | awk -F 'Python |</a>' '/<span class="release-number"><a[^>]+>Python '"$version"'./ { print $2 }' | sort -V | tail -1)"
 	(
 		set -x
 		sed -ri 's/^(ENV PYTHON_VERSION) .*/\1 '"$fullVersion"'/' "$version/Dockerfile"
