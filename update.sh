c24c7b5b7c77c99793c4cff488a81e8cb3cc7e61
Tianon Gravi <admwiggin@gmail.com>
2018-07-13 13:02:16 -0700

    Add 3.6/alpine3.8, 3.5/alpine3.8, and 3.4/alpine3.8

--- update.sh
+++ update.sh
@@ -161,20 +161,18 @@ for version in "${versions[@]}"; do
 			wheezy) sed -ri -e 's/dpkg-architecture --query /dpkg-architecture -q/g' "$dir/Dockerfile" ;;
 		esac
 
-		# https://bugs.python.org/issue32598 (Python 3.7.0b1+)
-		# TL;DR: Python 3.7+ uses OpenSSL functionality which LibreSSL 2.6.x in Alpine 3.7 doesn't implement
-		if [[ "$version" == 3.7* ]] && [[ "$variant" == alpine3.7 ]]; then
-			sed -ri -e 's/libressl/openssl/g' "$dir/Dockerfile"
-		fi
-
-		# Libraries to build the nis module only available in Alpine 3.7+.
-		# Also require this patch https://bugs.python.org/issue32521 only available in Python 2.7, 3.6+.
-		if [[ "$variant" == alpine* ]] && [[ "$version" == 3.4* || "$version" == 3.5* ]] \
-				|| [[ "$variant" == alpine3.6 ]]; then
-			sed -ri -e '/libnsl-dev/d' -e '/libtirpc-dev/d' "$dir/Dockerfile"
-		fi
-
 		case "$version/$v" in
+			# https://bugs.python.org/issue32598 (Python 3.7.0b1+)
+			# TL;DR: Python 3.7+ uses OpenSSL functionality which LibreSSL 2.6.x in Alpine 3.7 doesn't implement
+			# Python 3.5 on Alpine 3.8 needs OpenSSL too
+			3.7*/alpine3.7 | 3.5*/alpine3.8)
+				sed -ri -e 's/libressl/openssl/g' "$dir/Dockerfile"
+				;;& # (3.5*/alpine* needs to match the next block too)
+			# Libraries to build the nis module only available in Alpine 3.7+.
+			# Also require this patch https://bugs.python.org/issue32521 only available in Python 2.7, 3.6+.
+			3.4*/alpine* | 3.5*/alpine* | */alpine3.6)
+				sed -ri -e '/libnsl-dev/d' -e '/libtirpc-dev/d' "$dir/Dockerfile"
+				;;
 			3.4/stretch*)
 				sed -ri -e 's/libssl-dev/libssl1.0-dev/g' "$dir/Dockerfile"
 				;;
