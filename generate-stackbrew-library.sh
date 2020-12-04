0559964914e6603abb50ef17da03265ebb3646cd
Tianon Gravi <admwiggin@gmail.com>
2020-12-04 09:24:58 -0800

    Fix the Windows ordering in generate-stackbrew-library.sh
    See https://github.com/docker-library/mongo/pull/444 for more details.

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -80,7 +80,7 @@ for version in "${versions[@]}"; do
 	for v in \
 		{buster,stretch}{,/slim} \
 		alpine{3.12,3.11} \
-		windows/windowsservercore-{ltsc2016,1809} \
+		windows/windowsservercore-{1809,ltsc2016} \
 	; do
 		dir="$version/$v"
 		variant="$(basename "$v")"
