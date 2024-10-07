069d5b03bcbc84dd905a2b0d4a5eee89977e3197
Jesper Noordsij <jesper.noordsij@gmail.com>
2024-10-07 20:57:47 +0200

    Drop eol 3.8

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -137,7 +137,7 @@ for version; do
 		esac
 
 		case "$version" in
-			3.8 | 3.9) ;;
+			3.9) ;;
 			*)
 				if [ "$version" != '3.10' ]; then
 					# https://github.com/docker-library/python/pull/931
